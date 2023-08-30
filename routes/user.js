6dc721dceb026f418ba08a44758fe0315aa4503c
Ayom04 <abdullahayomide04@gmail.com>
2023-08-30 22:55:31 +0100

    completer swagger integration for users end points

--- routes/user.js
+++ routes/user.js
@@ -16,10 +16,10 @@ const authorization = require("../middleware/authorization");
  * @swagger
  * /users/create-user:
  *   post:
- *     summary: creates a new account
+ *     summary: creates a new user
  *     description: This Creates a new record for the user
  *     tags:
- *       - Account
+ *       - USERS
  *     produces:
  *       - application/json
  *     parameters:
@@ -63,10 +63,138 @@ const authorization = require("../middleware/authorization");
  *          Bad Request
  */
 router.post("/create-user", registerUser);
-router.patch("/update-user", authentication, authorization, updateUser);
+
+/**
+ * register a new user
+ * @swagger
+ * /users/login :
+ *   post:
+ *     summary: logs in a registered user
+ *     description: this is to allow the user to be login
+ *     tags:
+ *       - USERS
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email_address
+ *         in: body
+ *         required: true
+ *       - name: password
+ *         in: body
+ *         required: true
+ *     responses:
+ *        200:
+ *          description: Log in successful.
+ *        400:
+ *          Bad Request
+ */
 router.post("/login", logIn);
+
+/**
+ * register a new user
+ * @swagger
+ * /users/update-user :
+ *   patch:
+ *     summary: updates a registered user's profile
+ *     description: this enables the user to edit their profile
+ *     tags:
+ *       - USERS
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: surname
+ *         in: body
+ *         required: false
+ *       - name: othernames
+ *         in: body
+ *         required: false
+ *       - name: phone
+ *         in: body
+ *         required: false
+ *       - name: about_me
+ *         in: body
+ *         required: false
+ *       - name: occupation
+ *         in: body
+ *         required: false
+ *     responses:
+ *        200:
+ *          description: User details updated Successfully.
+ *        400:
+ *          Bad Request
+ */
+router.patch("/update-user", authentication, authorization, updateUser);
+
+/**
+ * register a new user
+ * @swagger
+ * /users/delete-user :
+ *   patch:
+ *     summary: verifies a user account
+ *     description: this is to allow the user to be verified before using the app.
+ *     tags:
+ *       - USERS
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: token
+ *         in: headers
+ *         required: true
+ *     responses:
+ *        200:
+ *          description: User deleted Succesfully.
+ *        400:
+ *          Bad Request
+ */
 router.patch("/delete-user", authentication, authorization, deleteUser);
+
+/**
+ * register a new user
+ * @swagger
+ * /users/verify/{email_address}/{otp} :
+ *   patch:
+ *     summary: logs in a registered user
+ *     description: this is to allow the user to be login
+ *     tags:
+ *       - USERS
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email_address
+ *         in: path
+ *         required: true
+ *       - name: otp
+ *         in: path
+ *         required: true
+ *     responses:
+ *        200:
+ *          description: User verified successfully.
+ *        400:
+ *          Bad Request
+ */
 router.patch("/verify/:email_address/:otp", verifyUser);
+
+/**
+ * register a new user
+ * @swagger
+ * /users/resend-otp/{email_address}:
+ *   post:
+ *     summary: resends otps to user.
+ *     description: this end point sends otp to the user to verify his.her account.
+ *     tags:
+ *       - USERS
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email_address
+ *         in: path
+ *         required: true
+ *     responses:
+ *        200:
+ *          description: Otp succesfully sent.
+ *        400:
+ *          Bad Request
+ */
 router.get("/resend-otp/:email_address", resendOtp);
 
 module.exports = router;
