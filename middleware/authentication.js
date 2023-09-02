b1364444803ab068da24a48a6ac86ad2abaf361a
Ayom04 <abdullahayomide04@gmail.com>
2023-09-02 11:06:40 +0100

    project completed

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,29 +1,27 @@
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
-const {unauthorisedAccess, serverError} = require('../constants/messages')
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { unauthorisedAccess, serverError } = require("../constants/messages");
 
+const authentication = (req, res, next) => {
+  const { authorization } = req.headers;
 
-const authentication = (req,res,next) =>{
-    const {authorization} = req.headers
+  try {
+    if (!authorization) throw new Error(unauthorisedAccess);
 
-    try {
-        if(!authorization)throw new Error(unauthorisedAccess)
+    const tokenSplit = authorization.split(" ");
 
-        const tokenSplit = authorization.split(' ')
+    jwt.verify(tokenSplit[1], process.env.JWT_SECRET, (err, decoded) => {
+      if (err) throw new Error(unauthorisedAccess);
 
-        jwt.verify(tokenSplit[1], process.env.JWT_SECRET, (err, decoded) =>{
-            if(err)throw new Error(unauthorisedAccess)
+      req.params.userEmail = decoded.email;
 
-            req.params.userEmail = decoded.email
-
-            next()
-
-        })
-    } catch (error) {
-        res.status(401).json({
-            status: false,
-            message: error.message || serverError
-        })
-    }
-}
-module.exports = authentication
+      next();
+    });
+  } catch (error) {
+    res.status(401).json({
+      status: false,
+      message: error.message || serverError,
+    });
+  }
+};
+module.exports = authentication;
